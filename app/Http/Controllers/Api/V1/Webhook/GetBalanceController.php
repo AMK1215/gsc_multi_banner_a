9b8a96a00cfd25d1a64882388bb5bbe43f203066
AMK1215 <aungmyokhaing1215@gmail.com>
2025-02-28 13:28:23 +0630

    userseeder update

--- app/Http/Controllers/Api/V1/Webhook/GetBalanceController.php
+++ app/Http/Controllers/Api/V1/Webhook/GetBalanceController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Api\V1\Webhook;
 
 use App\Enums\SlotWebhookResponseCode;
@@ -13,29 +14,16 @@ class GetBalanceController extends Controller
 {
     public function getBalance(SlotWebhookRequest $request)
     {
-        Log::info('GetBalanceController: Received request', ['request' => $request->all()]);
-
         DB::beginTransaction();
         try {
-            Log::info('GetBalanceController: Validating request');
-
             $validator = SlotWebhookValidator::make($request)->validate();
+
             if ($validator->fails()) {
                 Log::warning('GetBalanceController: Validation failed', ['errors' => $validator->getResponse()]);
                 return $validator->getResponse();
             }
 
-            // if ($validator->fails()) {
-            //     Log::warning('GetBalanceController: Validation failed', ['errors' => $validator->errors()]);
-            //     return $validator->getResponse();
-            // }
-
             $balance = $request->getMember()->balanceFloat;
-
-            Log::info('GetBalanceController: Balance fetched successfully', [
-                'member_id' => $request->getMember()->id,
-                'balance' => $balance,
-            ]);
 
             DB::commit();
 
@@ -46,11 +34,7 @@ class GetBalanceController extends Controller
             );
         } catch (\Exception $e) {
             DB::rollBack();
-
-            Log::error('GetBalanceController: Exception occurred', [
-                'message' => $e->getMessage(),
-                'trace' => $e->getTraceAsString(),
-            ]);
+            Log::error('GetBalanceController: Exception occurred', ['message' => $e->getMessage(), 'trace' => $e->getTraceAsString()]);
 
             return response()->json([
                 'message' => $e->getMessage(),
@@ -58,44 +42,3 @@ class GetBalanceController extends Controller
         }
     }
 }
-
-
-// namespace App\Http\Controllers\Api\V1\Webhook;
-
-// use App\Enums\SlotWebhookResponseCode;
-// use App\Http\Controllers\Controller;
-// use App\Http\Requests\Slot\SlotWebhookRequest;
-// use App\Services\Slot\SlotWebhookService;
-// use App\Services\Slot\SlotWebhookValidator;
-// use Illuminate\Support\Facades\DB;
-
-// class GetBalanceController extends Controller
-// {
-//     public function getBalance(SlotWebhookRequest $request)
-//     {
-//         DB::beginTransaction();
-//         try {
-//             $validator = SlotWebhookValidator::make($request)->validate();
-
-//             if ($validator->fails()) {
-//                 return $validator->getResponse();
-//             }
-
-//             $balance = $request->getMember()->balanceFloat;
-
-//             DB::commit();
-
-//             return SlotWebhookService::buildResponse(
-//                 SlotWebhookResponseCode::Success,
-//                 $balance,
-//                 $balance
-//             );
-//         } catch (\Exception $e) {
-//             DB::rollBack();
-
-//             return response()->json([
-//                 'message' => $e->getMessage(),
-//             ]);
-//         }
-//     }
-// }
